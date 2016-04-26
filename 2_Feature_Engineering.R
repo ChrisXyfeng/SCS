c19a76e6a9b18e2cea4dcdd9f63e53f2a304bfa0
ChrisXyfeng <xyfengchris@gmail.com>
2016-04-26 21:33:56 +0200

    Update and rename Feature_Engineering.R to 2_Feature_Engineering.R

---
+++ 2_Feature_Engineering.R
@@ -0,0 +1,10 @@
+train <- as.data.frame(fread("train_pre.csv", integer64 = 'numeric'))
+test <- as.data.frame(fread("test_pre.csv", integer64 = 'numeric'))
+
+#############################Feature Engineering###############################
+###########Calculate the unique In Excel###################
+
+vars <- colnames(train)
+vars <- cbind(vars,apply(train, 2, function(x) length(unique(x))))
+colnames(vars) <- c('Vars','Unique')
+#write.csv(vars, 'Features_Engineering.csv',row.names = FALSE)
