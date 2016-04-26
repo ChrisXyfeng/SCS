ea2821fa832a7edd4f8ff8c763af71cc3dcf5cd3
ChrisXyfeng <xyfengchris@gmail.com>
2016-04-26 22:31:06 +0200

    Update 3_Raw_Data_Model.R

--- 3_Raw_Data_Model.R
+++ 3_Raw_Data_Model.R
@@ -3,7 +3,7 @@ require(data.table)
 require(Matrix)
 rm(list = ls())
 gc(reset = TRUE)
-#Models on raw data
+
 train <- as.data.frame(fread("train_pre.csv", integer64 = 'numeric'))
 test <- as.data.frame(fread("test_pre.csv", integer64 = 'numeric'))
 #Split data
@@ -17,20 +17,27 @@ label_cal <- as.factor(cal$TARGET)
 train$TARGET <- cal$TARGET <- NULL
 
 #Sparse Matrix
-train <- sparse.model.matrix(~., data = train)
-cal <- sparse.model.matrix(~., data = cal)
+# train <- sparse.model.matrix(~., data = train)
+# cal <- sparse.model.matrix(~., data = cal)
 #Matrix
 train <- as.matrix(train)
 cal <- as.matrix(cal)
 
+library(ROCR)
+calcAUC <- function(predcol,outcol) {
+    perf <- performance(prediction(predcol,outcol==pos),'auc')
+    as.numeric(perf@y.values)
+}
 #Models in Caret
 control_clf <- trainControl(method = "cv",
-                            number = 5,
-                            savePredictions = TRUE)
-grid_rf_1 <- expand.grid(mtry=1)
+                            number = 2,
+                            savePredictions = TRUE,
+                            summaryFunction = calcAUC)
+# grid_rf_1 <- expand.grid(mtry=1)
 set.seed(130622)
 clf_rf_1 <- train(x=train, y=label_train,
-                  method = "rf",
+                  method = "xgbTree",
+                  metric = calcAUC,
                   trControl = control_clf,
-                  verbose = FALSE,
-                  tunegrid = grid_rf_1)
+                  verbose = TRUE,
+                  tuneLength = 1)
