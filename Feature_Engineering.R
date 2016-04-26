db6c6cd947ba48a5cf1b839587b8097e94bfbaf1
ChrisXyfeng <xyfengchris@gmail.com>
2016-04-26 15:00:31 +0200

    Update Feature_Engineering.R

--- Feature_Engineering.R
+++ Feature_Engineering.R
@@ -1,23 +1,23 @@
 train <- read.csv("train_pre.csv")
 test <- read.csv("test_pre.csv")
 #############################Feature Engineering###############################
+###########Calculate the unique In Excel###################
 
-#Calculate the unique
-
-vars <- colnames(train)
-vars <- cbind(vars,apply(train, 2, function(x) length(unique(x))))
-colnames(vars) <- c('Vars','Unique')
-write.csv(vars, 'Features_Engineering.csv',row.names = FALSE)
-
-library(gmodels)
-table(train$TARGET, train$saldo_medio_var17_ult1)
-CrossTable(train$TARGET, train$saldo_var6, chisq = TRUE)
-
+# vars <- colnames(train)
+# vars <- cbind(vars,apply(train, 2, function(x) length(unique(x))))
+# colnames(vars) <- c('Vars','Unique')
+# write.csv(vars, 'Features_Engineering.csv',row.names = FALSE)
+# 
+# library(gmodels)
+# table(train$TARGET, train$saldo_medio_var17_ult1)
+# CrossTable(train$TARGET, train$saldo_var6, chisq = TRUE)
+###########################################################
+#Categorical Features
 vars <- read.csv('Features_Engineering.csv')
 for (i in 1:313) {
-    if (vars[i,'Feature']=='as.factor') {
-        train[,i] <- as.factor(train[,i])
-        test[,i] <- as.factor(test[,i])
-    }
+  if (vars[i,'Feature']=='as.factor') {
+    train[,i] <- as.factor(train[,i])
+    test[,i] <- as.factor(test[,i])
+  }
 }
-#Modify in the excel
+
